35d1b0801f15aca29b3da0af3f3b786ef541c877
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-15 20:52:18 +0000

    add initial html structure with Audioplayer and Stationlist classes

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import AudioPlayer from './AudioPlayer.jsx';
+import StationList from './StationList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stations: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/stations')
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        this.setState({ stations: json })
+        console.log('parsed json', json)
+      }).catch((ex) => {
+        console.log('parsing failed', ex)
+      });
+    console.log("Mounted");
+  }
+
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <header>
+          <h1>Welcome to Odd Radio - Your link to curated, independent, Great, Canadian radio</h1>
+        </header>
+          <StationList stations={ this.state.stations } />
+          <div className="station-container">
+            <header>
+              <span className="station-name">NAME</span>
+              <button className="station-play-button">play</button>
+              <button className="station-info-button">info</button>
+            </header>
+            <main>
+              <span className="station-location">Location:</span>
+              <span className="station-description">Description:</span>
+              <span className="station-link">Link:</span>
+              <span className="station-stream">Stream: http://onair.cfcr.ca/hifi.mp3</span>
+            </main>
+          </div>
+        <footer>
+           <AudioPlayer />
+        </footer>
+      </div>
+    );
   }
 }
+export default App;
