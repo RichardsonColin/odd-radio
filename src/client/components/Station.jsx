d3dd82948721aa032c13a135688c268ae72596ee
Sebastian Bethell <sebbethell@gmail.com>
2017-12-15 20:59:43 +0000

    updated classNames in Station to match skeleton from App

--- src/client/components/Station.jsx
+++ src/client/components/Station.jsx
@@ -5,13 +5,13 @@ class Station extends Component {
 render() {
     console.log('from station!!')
   return (
-    <div className="container individual-station-container">
+    <div className="container station-container">
       <div className="row station-row border">
         <div className="one-third column station-name center"> {this.props.name} </div>
-        <div className="one-third column play-btn center">
+        <div className="one-third column station-play-button center">
           <i className="fa fa-play" aria-hidden="true"></i>
         </div>
-        <div className="one-third column dot-info center">
+        <div className="one-third column station-info-button center">
           <i className="fa fa-chevron-down" aria-hidden="true"></i>
         </div>
       </div>
