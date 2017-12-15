bd0837fdeaebedd923fe5957513f19f08f7a17fd
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-15 21:17:19 +0000

    update syntax

--- src/client/components/Station.jsx
+++ src/client/components/Station.jsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class Station extends Component {
 
-render() {
-    console.log('from station!!')
-  return (
-    <div className="container station-container">
-      <div className="row station-row border">
-        <div className="one-third column station-name center"> {this.props.name} </div>
-        <div className="one-third column station-play-button center">
-          <i className="fa fa-play" aria-hidden="true"></i>
-        </div>
-        <div className="one-third column station-info-button center">
-          <i className="fa fa-chevron-down" aria-hidden="true"></i>
+  render() {
+      console.log('from station!!')
+    return (
+      <div className="container station-container">
+        <div className="row station-row border">
+          <div className="one-third column station-name center"> {this.props.name} </div>
+          <div className="one-third column station-play-button center">
+            <i className="fa fa-play" aria-hidden="true"></i>
+          </div>
+          <div className="one-third column station-info-button center">
+            <i className="fa fa-chevron-down" aria-hidden="true"></i>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 export default Station;
