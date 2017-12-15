bb9560224d5c4637118734dac1a2068871251072
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-15 20:55:51 +0000

    add div for the audio player

--- src/client/components/AudioPlayer.jsx
+++ src/client/components/AudioPlayer.jsx
@@ -5,5 +5,16 @@ class AudioPlayer extends Component {
     super(props);
   }
 
+  render() {
+    return (
+      <div>
+        <div className="player-station-name">NAME</div>
+        <audio controls="controls" preload="none">
+          <source src="" type="audio/mp3" />
+        </audio>
+        <div className="seek-button"><button>seek</button></div>
+      </div>
+    );
+  }
 }
 export default AudioPlayer;
