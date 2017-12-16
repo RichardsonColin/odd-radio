7d18fdd79bf7e4bfff54a2dc773beea2c02ba039
Johnnie Regalado <johnniereg@gmail.com>
2017-12-16 22:03:42 +0000

    Pulled functions out of Station Component.
    Put into a utility function file.

--- src/client/util/ClientFunctions.jsx
+++ src/client/util/ClientFunctions.jsx
@@ -1,2 +1,20 @@
 import React, { Component } from 'react';
 
+export const onStationSelect = function(event) {
+    this.props.handleSelectedStation(this.state.name, this.state.stream, this.state.type);
+    
+}
+
+export const onInfoSelect = function(event) {
+    console.log('---- onInfoSelect ----', this);
+    let infoId = this.props.name
+    let info = document.getElementById(infoId);
+    console.log('info.className', info.className)
+    if (info.className === "container station-container content") {
+        info.className = "";
+        info.className = "container station-container";
+    } else {
+        info.className = "";
+        info.className = "container station-container content";
+    }
+}
