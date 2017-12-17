a0887d644ec15f48d149bfb200d75e89cd38811c
DevYves <yfreypons@gmail.com>
2017-12-17 22:33:02 +0000

    Added responsive media queries for footer and fixed container
    These changes keep the footer from hiding any of the divs by changing the margin of the station list container depending on the screen size. Also changed app container to position absolute and width 100% to make colour scroll reach bottom of window.

--- src/client/components/Station.jsx
+++ src/client/components/Station.jsx
@@ -18,15 +18,16 @@ class Station extends Component {
   render() {
       console.log('Rendering <Station>')
     return (
+
       <div>
         <div className="container station-container">
           <div className="row station-row border">
             <div className="one-third column station-name center"> {this.props.name} </div>
             <div className="one-third column station-play-button center">
-              <i className="fa fa-play" aria-hidden="true" onClick={ this.onStationSelect }></i>
+              <i className="fa fa-play card-play-button " aria-hidden="true" onClick={ this.onStationSelect }></i>
             </div>
             <div className="one-third column station-info-button center">
-              <i className="fa fa-chevron-down" aria-hidden="true" onClick={ this.onInfoSelect }></i>
+              <i className="fa fa-chevron-down card-chevron" aria-hidden="true" onClick={ this.onInfoSelect }></i>
             </div>
           </div>
         </div>
--- src/client/util/ClientFunctions.jsx
+++ src/client/util/ClientFunctions.jsx
@@ -146,3 +146,5 @@ export const scrollListener = function() {
   });
 }
 
+
+
