aa8d754251633afef4ae5a07b10af8d8964cc9b1
Sebastian Bethell <sebbethell@gmail.com>
2017-12-17 21:38:18 +0000

    updated index.js for trying to connect heroku to db

--- src/server/index.js
+++ src/server/index.js
@@ -24,6 +24,18 @@ app.get('/api/stations/:id', (req, res) => {
   });
 })
 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 app.listen(process.env.PORT || 3000) //listens on port 3000 -> http://localhost:3000/
 console.log('listening on port env.PORT or 3000');
