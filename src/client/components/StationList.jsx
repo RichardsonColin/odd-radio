8d14ce52827ccbe0ce483b47af5b3af63b28d36a
Johnnie Regalado <johnniereg@gmail.com>
2017-12-21 20:38:25 +0000

    Refactor conditionals.
    Swapping variables outside div returns. Reducing code and possible errors.

--- src/client/components/StationList.jsx
+++ src/client/components/StationList.jsx
@@ -9,46 +9,32 @@ class StationList extends Component {
 
   render() {
     const station = this.props.stations.map((post, index) => {
-      let isActive;
+      let isActive = false;
+      let divStyle = 2;
+
       if (post.id === this.props.activeStation) {
         isActive = true;
-      } else {
-        isActive = false;
       }
 
       if (index % 2 === 0) {
-        return <Station
-          key={ post.id }
-          id={ post.id }
-          name={ post.name }
-          description={ post.description }
-          audioFeed={ post.audio_feed }
-          streamType={ post.stream_type }
-          homePage={ post.home_page }
-          city={ post.city }
-          handleSelectedStation={ this.props.handleSelectedStation }
-          stationType={1}
-          activeStation={ isActive }
-          playState={ this.props.playState }
-          streamLoading={ this.props.streamLoading }
-           />
-      } else {
-        return <Station
-          key={ post.id }
-          id={ post.id }
-          name={ post.name }
-          description={ post.description }
-          audioFeed={ post.audio_feed }
-          streamType={ post.stream_type }
-          homePage={ post.home_page }
-          city={ post.city }
-          handleSelectedStation={ this.props.handleSelectedStation }
-          stationType={2}
-          activeStation={ isActive }
-          playState={ this.props.playState }
-          streamLoading={ this.props.streamLoading }
-           />
+        divStyle = 1;
       }
+      
+      return <Station
+        key={ post.id }
+        id={ post.id }
+        name={ post.name }
+        description={ post.description }
+        audioFeed={ post.audio_feed }
+        streamType={ post.stream_type }
+        homePage={ post.home_page }
+        city={ post.city }
+        handleSelectedStation={ this.props.handleSelectedStation }
+        activeStation={ isActive }
+        playState={ this.props.playState }
+        streamLoading={ this.props.streamLoading }
+        stationType={ divStyle }
+      />
     });
 
     return (
