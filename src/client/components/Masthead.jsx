6f300f0230bb131b36f036461033a339337f8d07
Johnnie Regalado <johnniereg@gmail.com>
2018-01-07 18:50:57 +0000

    Component for Masthead

---
+++ src/client/components/Masthead.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Masthead extends Component {
+  render(){
+    return(
+    <div>
+      <div>
+        Masthead Goes Here
+      </div> 
+    </div>
+    );
+  }
+}
+
+export default Masthead;
