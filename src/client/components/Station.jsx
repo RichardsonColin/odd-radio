61e6debd11ae62c2a40f7b66202d433a5d515f36
Johnnie Regalado <johnniereg@gmail.com>
2018-01-10 00:26:15 +0000

    Styling

--- src/client/components/Station.jsx
+++ src/client/components/Station.jsx
@@ -57,7 +57,7 @@ componentWillReceiveProps(props) {
               </div>
               <div className="eight columns station-details">
                 <div className="description">{ this.props.description } </div>
-                <div className="location">{ this.props.city } </div>
+                <div className="location">{ this.props.city }, {this.props.province} </div>
                 <div className="homepage"><a href={this.props.homePage} target='_blank'>Homepage</a> </div>
               </div>
             </div>
