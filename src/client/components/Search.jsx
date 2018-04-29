dd6d64be39c0009843ec976aa283aebc664511e4
Yves Freypons <yfreypons@gmail.com>
2018-04-29 15:59:27 -0700

    Create search component

---
+++ src/client/components/Search.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+class Search extends Component {
+  searchParams(){
+    const searchValue = this.searchValue.value;
+    this.props.searchUpdate(searchValue);
+  }
+
+  render() {
+    console.log("search filter", this.props.searchFilter);
+    return (
+      <div className="search-container u-full-width">
+        <form className="search-form u-full-width">
+        <input className="search-input" type="text" ref={ (value) => this.searchValue = value } placeholder="Search" onChange={this.searchParams.bind(this)} />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search;
+
+
